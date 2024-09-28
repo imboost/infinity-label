7b631819616bad0da82090750dd48433d56b4e92
Taufik Sulaeman P <taufiksu@gmail.com>
2024-09-28 11:41:49 +0700

    1. Fix Load 2D Code

--- app.js
+++ app.js
@@ -188,21 +188,19 @@ app.post('/barcode', textParser, async (req, res) => {
     const component = req.body;
 
     var info = JSON.parse(component);
-    // console.log('info',info);
 
-    const barcode = bwipjs.toBuffer({
-        bcid:        info.type_code,       // Barcode type
-        text:        info.text,       // Text to encode
-        scale:       3,               // 3x scaling factor
-        height:      10,              // Bar height, in millimeters
+    bwipjs.toBuffer({
+        bcid: info.type_code,       // Barcode type
+        text: info.text,       // Text to encode
+        scale: 3,               // 3x scaling factor
+        height: 10,              // Bar height, in millimeters
         includetext: true,            // Show human-readable text
-        textxalign:  'center',        // Always good to set this
+        textxalign: 'center',        // Always good to set this
     }, function (err, png) {
         if (err) {
-            // `err` may be a string or Error object
+            console.log(err);
         } else {
             res.status(200).json(png.toString('base64'));
-            // console.log('pngNYA', png);
             // `png` is a Buffer
             // png.length           : PNG file length
             // png.readUInt32BE(16) : PNG image width
