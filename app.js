fcd9767f2afc272df6247217694fc83442859192
Taufik Sulaeman P <taufiksu@gmail.com>
2024-09-29 08:46:13 +0700

    1. Test

--- app.js
+++ app.js
@@ -189,13 +189,15 @@ app.post('/barcode', textParser, async (req, res) => {
 
     var info = JSON.parse(component);
 
+    console.log(info);
+
     bwipjs.toBuffer({
-        bcid: info.type_code,       // Barcode type
-        text: info.text,       // Text to encode
-        scale: 3,               // 3x scaling factor
-        height: 10,              // Bar height, in millimeters
-        includetext: true,            // Show human-readable text
-        textxalign: 'center',        // Always good to set this
+        bcid: info.type_code, // Barcode type
+        text: info.text, // Text to encode
+        scale: 3, // 3x scaling factor
+        height: 10, // Bar height, in millimeters
+        includetext: true, // Show human-readable text
+        textxalign: 'center', // Always good to set this
     }, function (err, png) {
         if (err) {
             console.log(err);
